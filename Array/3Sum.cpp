d9775cba88b5e001885575cc0827fa07529a9e18
Dhruba Datta <dhrubadatta30@gmail.com>
2026-09-19 10:49:37 +0530

    Update 3Sum.cpp

--- Array/3Sum.cpp
+++ Array/3Sum.cpp
@@ -12,13 +12,14 @@ public:
 
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
-                if(m[-(nums[i]+nums[j])]==1)
+                int target=-(nums[i]+nums[j]);
+                if(m.find(target)!=m.end())
                     temp.insert(triplet(nums[i],nums[j],(-nums[i]-nums[j])));
                 m[nums[j]]=1;    
             }
             m.clear();
         }
-        vector<vector<int>> v={temp.begin(),temp.end()};
+        vector<vector<int>> v(temp.begin(),temp.end());
         return v;
     }
 };
