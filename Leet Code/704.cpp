332514ba5e3a141df8b1f04ae666a114cecc3ec6
Dhruba Datta <dhrubadatta30@gmail.com>
2026-09-20 00:03:43 +0530

    Implement binary search in 704.cpp

---
+++ Leet Code/704.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,h=nums.size()-1;
+        int mid=(l+h)/2;
+
+        while(l<=h){
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) h=mid-1;
+            else l=mid+1;
+            mid=(l+h)/2;
+        }
+        return -1;
+    }
+};
