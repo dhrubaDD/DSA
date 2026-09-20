ae61cda489c3180417599a0ed144e12bcba94f34
Dhruba Datta <dhrubadatta30@gmail.com>
2026-09-20 21:27:30 +0530

    Add search method for rotated array
    Implement binary search for rotated sorted array.

---
+++ Leet Code/33.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low=0, high=nums.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>nums[low]){
+                if(nums[low]==target) return low;
+                else if(nums[low]<target && target<nums[mid]) high=mid-1;
+                else low=mid+1;
+            }
+            else{
+                if(nums[high]==target) return high;
+                else if(nums[mid]<target && target<nums[high]) low=mid+1;
+                else high=mid-1;
+            }
+        }
+        return -1;
+
+    
+    }
+}
